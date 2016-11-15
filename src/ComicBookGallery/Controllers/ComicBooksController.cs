3b82846c2cbb13884f35f5727866d8d6605dcba8
Murray <Murray@TESLA>
2016-11-14 21:53:05 -0500

    Test

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,17 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public String Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller"
+            };
+            
         }
     }
 }
