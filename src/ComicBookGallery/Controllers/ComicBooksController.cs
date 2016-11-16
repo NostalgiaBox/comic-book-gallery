e79da15e8990b7ebb403599e386e30040a0dce69
Murray <Murray@TESLA>
2016-11-15 20:37:24 -0500

    Added comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,14 +10,8 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return new ContentResult()
-            {
-                Content = "Hello from the comic books controller"
-            };
+
+            return View();
             
         }
     }
